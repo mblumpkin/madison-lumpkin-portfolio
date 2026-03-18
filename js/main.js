ea97fea73f285fc9caa41cdd62512a3842e0c528
mblumpkin <mblumpkin2@gmail.com>
2026-03-18 10:52:44 -0500

    added sections and images
    added software and language logos, fixed modal for images, working on illustration section

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ function resizeAll(){
 
 window.addEventListener('load', resizeAll);
 window.addEventListener('resize', resizeAll);
+
+function openModal(title, description, image){
+    document.getElementById("modal").style.display = "flex";
+
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalText").innerText = description;
+    document.getElementById("modalImg").src = image;
+}
+
+function closeModal(){
+    document.getElementById("modal").style.display = "none";
+}
