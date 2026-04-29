813e6def2ccc52d47a46f5cbc21ed0d19c918237
mblumpkin <mblumpkin2@gmail.com>
2026-04-29 13:05:53 -0500

    fixing alt atributes
    adding alts to images

--- js/main.js
+++ js/main.js
@@ -116,3 +116,12 @@ function closeModal(){
 
       document.body.classList.remove("modal-open");
 }
+
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".nav-pill");
+    if (window.scrollY > 50) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
